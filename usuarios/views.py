137944678c2daa59e0b18d9c6a862cfb770b0d14
Felipe <105012588+febanqueri@users.noreply.github.com>
2023-04-11 16:22:37 -0300

    Cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 
 # Create your views here.
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        
+        if not (senha == confirmar_senha):    
+            messages.add_message(request, constants.ERROR, 'As senhas não iguais.')
+            return redirect(reverse('cadastro'))
+        
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'Usuário já existe.')
+            return redirect(reverse('cadastro'))   
+        
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        user.save()
+        messages.add_message(request, constants.SUCCESS, 'Usuário salvo com sucesso.')
+        return redirect(reverse('login'))
